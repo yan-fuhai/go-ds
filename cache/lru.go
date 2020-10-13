7ea44d33a0721c32c248df2376191b578048c3c0
yanfuhai <yan_fuhai@foxmail.com>
2020-10-13 09:40:42 +0800

    Add doc for LRUCache.

--- cache/lru.go
+++ cache/lru.go
@@ -7,6 +7,8 @@ type doubleListNode struct {
 	right *doubleListNode
 }
 
+// LRUCache store key-value pairs with fixed capacity.
+// It would remove the least recently used (LRU) key-value pair when it reach the capacity.
 type LRUCache struct {
 	capacity int
 	head     *doubleListNode
